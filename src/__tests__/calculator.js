8bbeffe1fa26764dc1dde07b66bbc4400b1d453e
David Inoa <davidinoa@gmail.com>
2020-01-01 08:55:11 -0500

    Update Jest config

--- src/__tests__/calculator.js
+++ src/__tests__/calculator.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import AutoScalingText from '../auto-scaling-text'
-
+import Calculator from '../calculator'
 test('renders', () => {
-  render(<AutoScalingText />)
+  render(<Calculator />)
 })
